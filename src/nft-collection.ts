b73f187d3730a77afbc7e670705c9e91a9d2ab72
Tadej Vengust <vengust.tadej@gmail.com>
2023-12-27 14:55:55 +0100

    get collection

---
+++ src/nft-collection.ts
@@ -0,0 +1,20 @@
+import { CollectionType, EvmChain, Nft } from "@apillon/sdk";
+import { EnvLevel, prepareEnv } from "./helpers";
+
+(async () => {
+  prepareEnv(EnvLevel.NFT);
+  const nft = new Nft({
+    key: process.env.APILLON_API_KEY,
+    secret: process.env.APILLON_API_SECRET,
+  });
+
+  const collection = await nft.collection(process.env.APILLON_COLLECTION).get();
+
+  console.log("Status: ", collection);
+  console.log("Contract address: ", collection.contractAddress);
+  console.log(
+    "For more info on statuses check: https://sdk-docs.apillon.io/enums/CollectionStatus.html"
+  );
+})().catch(async (err) => {
+  console.log(err);
+});
