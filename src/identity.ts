e69a97d38cdb1670304003bf33267eff31197093
Tadej Vengust <vengust.tadej@gmail.com>
2023-12-27 14:27:07 +0100

    get wallet identity

--- src/identity.ts
+++ src/identity.ts
@@ -1,5 +1,17 @@
 import { Identity } from "@apillon/sdk";
+import { prepareEnv } from "./helpers";
 
-const identity = new Identity();
-const message = identity.generateSigningMessage("test");
-console.log(message.message);
+(async () => {
+  prepareEnv();
+  const identity = new Identity({
+    key: process.env.APILLON_API_KEY,
+    secret: process.env.APILLON_API_SECRET,
+  });
+  // check any dot wallet you want
+  const info = await identity.getWalletIdentity(
+    "5HB6TahxS9KpSAq69tqjvU7VLuzKsVkCpPSPULEYixrqvn1V"
+  );
+  console.log(info);
+})().catch(async (err) => {
+  console.log(err);
+});
