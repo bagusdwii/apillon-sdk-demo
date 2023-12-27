4f34f221f3abba757e6ae467b1860cf651dc9e25
Tadej Vengust <vengust.tadej@gmail.com>
2023-12-27 12:38:04 +0100

    helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,30 @@
 import * as dotenv from "dotenv";
 
-export function prepareEnv() {
+export enum EnvLevel {
+  GENERAL = 1,
+  STORAGE = 2,
+  STORAGE_IPNS = 3,
+}
+
+export function prepareEnv(level: EnvLevel = EnvLevel.GENERAL) {
   dotenv.config();
+
   if (!process.env.APILLON_API_KEY) {
     throw new Error("Missing env: APILLON_API_KEY");
+  }
+
+  if (!process.env.APILLON_API_SECRET) {
+    throw new Error("Missing env: APILLON_API_SECRET");
+  }
+
+  if (
+    (level == EnvLevel.STORAGE || level == EnvLevel.STORAGE_IPNS) &&
+    !process.env.APILLON_BUCKET
+  ) {
+    throw new Error("Missing env: APILLON_BUCKET");
+  }
+
+  if (level == EnvLevel.STORAGE_IPNS && !process.env.APILLON_IPNS) {
+    throw new Error("Missing env: APILLON_IPNS");
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,14 @@ We have prepared multiple script for you to run so you can see Apillon SDK in ac
 You will need to send ENV variables with you API key and secret to run examples.
 If you are running in codesandbox be careful of exposing the keys.
 
-We have all major functionalites as examples so you can run scripts:
+We have all major functionalities as examples so you can run scripts:
 - npm run storage:buckets
 - npm run storage:bucket-content
 - npm run storage:bucket-upload
+- npm run storage:bucket-ipns-create
+- npm run storage:bucket-ipns-update
+
+- npm run hosting:
 
 - npm run identity:generate-signing-message
 
